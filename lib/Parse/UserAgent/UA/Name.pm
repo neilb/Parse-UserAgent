79f9569f03484c24898459fd35269e10256f3d70
Neil Bowers <neil@bowers.com>
2013-04-24 22:46:23 +0100

    Added cmp overloading so name and version can be used in eq tests

--- lib/Parse/UserAgent/UA/Name.pm
+++ lib/Parse/UserAgent/UA/Name.pm
@@ -1,7 +1,14 @@
 package Parse::UserAgent::UA::Name;
 
 use Moo;
-use overload '""' => sub { $_[0]->cooked(); };
+use overload
+    '""'  => sub { $_[0]->cooked(); },
+    'cmp' => sub {
+                my ($self, $other, $swap) = @_;
+                my $result = $self->raw cmp $other;
+                $result = -$result if $swap;
+                return $result;
+             };
 
 has raw    => ( is => 'ro');
 has cooked => ( is => 'ro');
--- lib/Parse/UserAgent/UA/Version.pm
+++ lib/Parse/UserAgent/UA/Version.pm
@@ -1,7 +1,14 @@
 package Parse::UserAgent::UA::Version;
 
 use Moo;
-use overload '""' => sub { $_[0]->raw(); };
+use overload
+    '""'  => sub { $_[0]->raw(); },
+    'cmp' => sub {
+                my ($self, $other, $swap) = @_;
+                my $result = $self->raw cmp $other;
+                $result = -$result if $swap;
+                return $result;
+             };
 
 has raw   => (is => 'ro');
 has major => (is => 'ro');
